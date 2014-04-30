8dfbf0e5eac828481f20de157e6e14075a2b0f7e
Steve Huff <steve.huff@runkeeper.com>
2014-04-30 16:52:23 -0400

    verify that rbenv can build gems with binary components

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,4 +2,10 @@ class { 'rbenv': } ->
 rbenv::plugin { 'sstephenson/ruby-build': } ->
 rbenv::build { [ '1.9.3-p545', '2.0.0-p451' ]:
   global => true,
+} ->
+rbenv::gem { 'rdiscount':
+  ruby_version => '1.9.3-p545',
+} ->
+rbenv::gem { 'rdiscount':
+  ruby_version => '2.0.0-p451',
 }
